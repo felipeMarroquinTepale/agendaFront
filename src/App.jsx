96e1829463fa2cd89857e013da0d4b34e5933a56
felipeMarroquinTepale <felipe.01tepa@gmail.com>
2023-03-10 22:12:42 -0600

    creando proyecto react con material UI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      <Button>
+        HOLA
+      </Button>
+    </div>
+  );
+}
+
+export default App;
